96b1f57d130f97c3be3e2acf63f12d92b0c92877
YoniMoonbean <beck.andersson@hotmail.se>
2026-07-04 19:43:06 +0200

    Create Vibes.js

---
+++ scripts/Vibes.js
@@ -0,0 +1,4 @@
+const vibes = [
+  "Ancient", "Futuristic", "Mystical", "Post-Apocalyptic",
+  "Cyberpunk", "Steampunk", "Dreamlike", "Haunted", "Magical", "Dystopian", "Tropical", "Overgrown", "Utopian", "Eerie", "Occult"
+];
